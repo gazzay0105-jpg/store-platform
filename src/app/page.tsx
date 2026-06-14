020e0832b316a5f9c951327d9f5e5f03ddf4b266
gazzay0105-jpg <g.azzay0105@gmail.com>
2026-06-14 22:10:27 +0800

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{padding: '40px', textAlign: 'center'}}>
+      <h1>Store Platform</h1>
+      <p>Монголын дэлгүүрийн платформ</p>
+      <a href="/auth/login">Нэвтрэх</a>
+    </div>
+  )
+}
